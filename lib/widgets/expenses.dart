b6f2b32c11db2950c6ee20fc1825ff03c3445539
teslacodes7 <satvik.manchanda@univest.in>
2026-03-04 12:11:06 +0530

    made expenses dismissible

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -80,6 +80,15 @@ class _ExpensesState extends State<Expenses> {
     });
   }
 
+
+  void _removeExpense(Expense expense){
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -92,7 +101,7 @@ class _ExpensesState extends State<Expenses> {
       body: Column(
       children: [
         const Text("The chart"),
-        Expanded(child: ExpensesList(expenses: _registeredExpenses),),
+        Expanded(child: ExpensesList(expenses: _registeredExpenses, onRemoveExpense: _removeExpense),),
       ],
     ),);
   }
